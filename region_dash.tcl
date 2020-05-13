a0e00a26426d6c61f807beacdd2e538f2fba3231
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-05-13 11:20:49 -0400

    run dashes directly, no anaysis menu

--- region_dash.tcl
+++ region_dash.tcl
@@ -1,16 +1,18 @@
-ds9Cmd "-analysis $ds9_hack_root/dashes.ans"
+### ds9Cmd "-analysis $ds9_hack_root/dashes.ans"
 
 ttk::frame $ds9(main).quick.dashes
 grid $ds9(main).quick.dashes -row 0 -column $atcol -padx 5
 incr atcol
 
-button $ds9(main).quick.dashes.8x3 -text {8x3} -takefocus 0 -command {ds9Cmd "-analysis task dash8x3"} \
+# Note: & below to put in background so xpa isn't blocked
+
+button $ds9(main).quick.dashes.8x3 -text {8x3} -takefocus 0 -command {exec $env(HOME)/ds9_hacks/dashlist.py $ds9(title) 8 3 &} \
     -image [image create photo -file "$ds9_hack_root/UI/8_3.png"] -borderwidth 0 -pady 0
-button $ds9(main).quick.dashes.2x2 -text {8x3} -takefocus 0 -command {ds9Cmd "-analysis task dash2x2"} \
+button $ds9(main).quick.dashes.2x2 -text {8x3} -takefocus 0 -command {exec $env(HOME)/ds9_hacks/dashlist.py $ds9(title) 2 2 &} \
     -image [image create photo -file "$ds9_hack_root/UI/2_2.png"] -borderwidth 0 -pady 0
-button $ds9(main).quick.dashes.10x4 -text {8x3} -takefocus 0 -command {ds9Cmd "-analysis task dash10x4"} \
+button $ds9(main).quick.dashes.10x4 -text {8x3} -takefocus 0 -command {exec $env(HOME)/ds9_hacks/dashlist.py $ds9(title) 10 4 &} \
     -image [image create photo -file "$ds9_hack_root/UI/10_4.png"] -borderwidth 0 -pady 0
-button $ds9(main).quick.dashes.5x5 -text {8x3} -takefocus 0 -command {ds9Cmd "-analysis task dash5x5"} \
+button $ds9(main).quick.dashes.5x5 -text {8x3} -takefocus 0 -command {exec $env(HOME)/ds9_hacks/dashlist.py $ds9(title) 5 5 &} \
     -image [image create photo -file "$ds9_hack_root/UI/5_5.png"] -borderwidth 0 -pady 0
 
 grid $ds9(main).quick.dashes.8x3 -row 0 -column 0
