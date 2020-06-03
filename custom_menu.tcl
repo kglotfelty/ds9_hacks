a442222325c1e27b9b31b3d9561b04d239dedbad
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-06-03 18:52:19 -0400

    update ciao to 3rd row

--- custom_menu.tcl
+++ custom_menu.tcl
@@ -34,11 +34,6 @@ source $ds9_hack_root/image_scale.tcl
 # ----------------------------------------
 
 source $ds9_hack_root/image_processing.tcl
-## ----------------------------------------
-
-if { [info exists env(ASCDS_INSTALL)] } {
-    source $ds9_hack_root/ciao_commands.tcl
-}
 
 ## ------------------------
 
@@ -48,10 +43,21 @@ ttk::frame $ds9(main).hack.quick_two
 pack $ds9(main).hack.quick_two -side top -anchor w -pady 2
 incr atcol
 
-source $ds9_hack_root/frame_buttons.tcl
 source $ds9_hack_root/lut.tcl
 source $ds9_hack_root/bin_buttons.tcl
 source $ds9_hack_root/block_buttons.tcl
 source $ds9_hack_root/zoom_buttons.tcl
+source $ds9_hack_root/frame_buttons.tcl
 
 
+## ----------------------------------------
+
+set atcol 0
+
+ttk::frame $ds9(main).hack.quick_three 
+pack $ds9(main).hack.quick_three -side top -anchor w -pady 2
+incr atcol
+
+if { [info exists env(ASCDS_INSTALL)] } {
+    source $ds9_hack_root/ciao_commands.tcl
+}
