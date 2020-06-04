459fb322351cba05c5cb39866c6f0c9f0f8d9b5e
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-06-04 12:07:47 -0400

    add additional region buttons

--- region_shapes.tcl
+++ region_shapes.tcl
@@ -60,30 +60,18 @@ $ds9(main).hack.quick.shapes.at.m add command -label "1" \
 
 grid $ds9(main).hack.quick.shapes.at -row 0 -column 0
 
+# TODO: figure out open region 
+# TODO: figure out save region
 
+ttk::button $ds9(main).hack.quick.shapes.info -text {Region Info} \
+    -takefocus 0 -command {ds9Cmd "-region getinfo"} \
+    -image [image create photo -file "$ds9_hack_root/UI/region_info.png"]
 
-#~ ttk::frame $ds9(main).hack.quick.shapes
-#~ grid $ds9(main).hack.quick.shapes -row 0 -column $atcol -columnspan 1 -padx 5
-#~ incr atcol
+ttk::button $ds9(main).hack.quick.shapes.list -text {Region List} \
+    -takefocus 0 -command {ds9Cmd "-region list"} \
+    -image [image create photo -file "$ds9_hack_root/UI/region_list.png"]
 
-#~ ttk::button $ds9(main).hack.quick.shapes.circle -text {C} -takefocus 0 -command {my_change_marker circle} \
-    #~ -image [image create photo -file "$ds9_hack_root/UI/circle.png"] 
-#~ ttk::button $ds9(main).hack.quick.shapes.annulus -text {A} -takefocus 0 -command {my_change_marker annulus} \
-    #~ -image [image create photo -file "$ds9_hack_root/UI/annulus.png"]
-#~ ttk::button $ds9(main).hack.quick.shapes.ellipse -text {E} -takefocus 0 -command {my_change_marker ellipse} \
-    #~ -image [image create photo -file "$ds9_hack_root/UI/ellipse.png"]
-#~ ttk::button $ds9(main).hack.quick.shapes.box -text {B} -takefocus 0 -command {my_change_marker box} \
-    #~ -image [image create photo -file "$ds9_hack_root/UI/box.png"]
-#~ ttk::button $ds9(main).hack.quick.shapes.poly -text {P} -takefocus 0 -command {my_change_marker polygon} \
-    #~ -image [image create photo -file "$ds9_hack_root/UI/poly.png"]
-#~ ttk::button $ds9(main).hack.quick.shapes.text -text {T} -takefocus 0 -command {my_change_marker text} \
-    #~ -image [image create photo -file "$ds9_hack_root/UI/text.png"]
+grid $ds9(main).hack.quick.shapes.info -row 0 -column 1
+grid $ds9(main).hack.quick.shapes.list -row 0 -column 2
 
-
-#~ grid $ds9(main).hack.quick.shapes.circle -row 0 -column 0
-#~ grid $ds9(main).hack.quick.shapes.annulus -row 0 -column 1
-#~ grid $ds9(main).hack.quick.shapes.ellipse -row 0 -column 2
-#~ grid $ds9(main).hack.quick.shapes.box -row 0 -column 3
-#~ grid $ds9(main).hack.quick.shapes.poly -row 0 -column 4
-#~ grid $ds9(main).hack.quick.shapes.text -row 0 -column 5
 
