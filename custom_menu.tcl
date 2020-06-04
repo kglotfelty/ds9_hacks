eb3828d84aa8f6de1106fd340ef6b8bdefcd43a6
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-06-04 09:01:20 -0400

    add power,quit

--- custom_menu.tcl
+++ custom_menu.tcl
@@ -9,31 +9,20 @@ ttk::frame $ds9(main).hack.quick
 pack $ds9(main).hack.quick -anchor w
 incr atcol
 
-# ----------------------------------------
+## ------------------------
 
 source $ds9_hack_root/region_shapes.tcl
-# ----------------------------------------
-
 source $ds9_hack_root/region_width.tcl
-# ----------------------------------------
-
 source $ds9_hack_root/region_dash.tcl
-# ----------------------------------------
-
 source $ds9_hack_root/region_state.tcl
-# ----------------------------------------
-
 source $ds9_hack_root/region_layer.tcl
-# ----------------------------------------
-
 source $ds9_hack_root/region_color.tcl
-# ----------------------------------------
-
-
 source $ds9_hack_root/image_scale.tcl
-# ----------------------------------------
-
 source $ds9_hack_root/image_processing.tcl
+
+ttk::button $ds9(main).hack.quick.quit -text {Quit} -takefocus 0 -command {ds9Cmd "-exit"} \
+    -image [image create photo -file "$ds9_hack_root/UI/quit.png"]
+grid $ds9(main).hack.quick.quit -row 0 -column $atcol -padx 15
 
 ## ------------------------
 
