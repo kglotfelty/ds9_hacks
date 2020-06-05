bca369018aea666711db73fc90b80d077763c60c
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2020-06-05 08:54:45 -0400

    add coordinate grid

--- image_processing.tcl
+++ image_processing.tcl
@@ -7,7 +7,10 @@ ttk::button $ds9(main).hack.quick.imgstuff.cntr -text {C} -takefocus 0 -command 
    -image [image create photo -file "$ds9_hack_root/UI/contour.png"]
 ttk::button  $ds9(main).hack.quick.imgstuff.wcs -text {WCS} -takefocus 0 -command {ds9Cmd "-match frame wcs" } \
    -image [image create photo -file "$ds9_hack_root/UI/match.png"]
+ttk::button  $ds9(main).hack.quick.imgstuff.cgrid -text {Coordinate Grid} -takefocus 0 -command {ds9Cmd "-grid yes" } \
+   -image [image create photo -file "$ds9_hack_root/UI/coord_grid.png"]
 
 grid $ds9(main).hack.quick.imgstuff.smth -row 0 -column 0 
 grid $ds9(main).hack.quick.imgstuff.cntr -row 0 -column 1 
-grid $ds9(main).hack.quick.imgstuff.wcs  -row 0 -column 2
+grid $ds9(main).hack.quick.imgstuff.cgrid -row 0 -column 2 
+grid $ds9(main).hack.quick.imgstuff.wcs  -row 0 -column 3
