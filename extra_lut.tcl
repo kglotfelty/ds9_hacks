0f859730deb308b6781d6478cf9c0e36ef725670
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2021-12-23 09:20:15 -0500

    updated for ds9 8.3 colorbar changes

--- extra_lut.tcl
+++ extra_lut.tcl
@@ -2,20 +2,66 @@
 
 proc MyLoadColormapFile { myid dir filename extn} {
     global colorbar
+    global icolorbar
 
-    if {$filename != {}} {
-        set lfile "$dir/$filename$extn"
-        set png ".png"
-        set pfile "$dir/$filename$png"
-        colorbar load "\{$lfile\}"
-        set id [colorbar get id]
-        set colorbar(map) [colorbar get name]
-        #set max_kjg_color $id
-        $myid add radiobutton \
-            -label "$colorbar(map)" \
-            -variable colorbar(map) -command "ChangeColormapID $id"  \
-            -image [image create photo -file "$pfile" ]
+    if {$filename == {}} {
+        return
     }
+
+    set lfile "$dir/$filename$extn"
+    set png ".png"
+    set pfile "$dir/$filename$png"
+
+    set fn $lfile
+
+    global colorbar
+    global icolorbar
+    global current
+    global ds9
+
+    if {$fn == {}} {
+	return
+    }
+
+    # first load into default cmap
+    set orgName [colorbar get name]
+    set orgInvert [colorbar get invert]
+
+    if {[catch {colorbar load "\{$fn\}"} rr]} {
+	Error $rr
+	return
+    }
+    set cmap [colorbar get name]
+    lappend icolorbar(user,cmaps) $cmap
+
+    colorbar map $orgName
+    colorbar invert $orgInvert
+
+    # now load into all current cmaps
+    foreach ff $ds9(frames) {
+	set cb ${ff}cb
+	switch [$cb get type] {
+	    base {
+		set orgName [$cb get name]
+		set orgInvert [$cb get invert]
+		if {[catch {$cb load "\{$fn\}"} rr]} {
+		    Error $rr
+		    return
+		}
+		$cb map $orgName
+		$cb invert $orgInvert
+	    }
+	    rgb {}
+	}
+    }
+
+    # add to menu
+    $myid add radiobutton \
+        -label $cmap \
+        -variable colorbar(map) \
+        -command [list ChangeColormapName $cmap] \
+        -image [image create photo -file "$pfile" ]
+
 }
 
 $ds9(mb).color add separator
