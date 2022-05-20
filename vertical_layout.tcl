8d202227d1600cc21a3d88e34e97039a79aafe38
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2022-05-20 07:35:24 -0400

    updat vertical layout

--- vertical_layout.tcl
+++ vertical_layout.tcl
@@ -1,54 +1,59 @@
 
-
-
 proc LayoutViewVert {} {
     global ds9
     global current
     global view
 
-    # canvas
+    # ds9(main) weight
+    grid rowconfigure $ds9(main) 4 -weight 0
+    grid columnconfigure $ds9(main) 0 -weight 0
     grid rowconfigure $ds9(main) 1 -weight 1
-    grid columnconfigure $ds9(main) 4 -weight 1
-    grid $ds9(image) -row 1 -column 4 
+    grid columnconfigure $ds9(main) 2 -weight 1
+
+    grid forget $ds9(panel)
+    grid forget $ds9(panel,sep)
+    grid forget $ds9(buttons,frame)
+    grid forget $ds9(buttons,sep)
+    grid forget $ds9(image)
+    
+    pack forget $ds9(magnifier)
+    pack forget $ds9(info)
+    pack forget $ds9(panner)
 
     # info panel
     if {$view(info) || $view(magnifier) || $view(panner)} {
-	grid $ds9(panel) -row 1 -column 0 -sticky ns
-	$ds9(panel,sep) configure -orient vertical
-	grid $ds9(panel,sep) -row 1 -column 1 -sticky ns
-    } else {
-	grid forget $ds9(panel)
-	grid forget $ds9(panel,sep)
-    }
-
-    if {$view(magnifier)} {
-	pack $ds9(magnifier) -side top -padx 2 -pady 2
-    } else {
-	pack forget $ds9(magnifier)
-    }
-
-    if {$view(info)} {
-	pack $ds9(info) -side top -padx 2 -pady 2 -fill y -expand true
-	if {$view(magnifier)} {
-	    pack $ds9(info) -after $ds9(magnifier)
-	}
-    } else {
-	pack forget $ds9(info)
+        grid $ds9(panel) -row 1 -column 3 -sticky ns
+        $ds9(panel,sep) configure -orient vertical
+        grid $ds9(panel,sep) -row 1 -column 4 -sticky ns
     }
 
     if {$view(panner)} {
-	pack $ds9(panner) -side bottom -padx 2 -pady 2
-    } else {
-	pack forget $ds9(panner)
+        pack $ds9(panner) -side top -padx 2 -pady 2
     }
+
+    if {$view(magnifier)} {
+        pack $ds9(magnifier) -side top -padx 2 -pady 2
+        if { $view(panner) } {
+            pack $ds9(info) -after $ds9(panner)
+        }
+    }
+
+    if {$view(info)} {
+        pack $ds9(info) -side top -padx 2 -pady 2 -fill y 
+        if {$view(magnifier)} {
+            pack $ds9(info) -after $ds9(magnifier)
+        }
+    }
+
 
     # buttons
     if {$view(buttons)} {
-	grid $ds9(buttons) -row 1 -column 2 -sticky ns
-	$ds9(buttons,sep) configure -orient vertical
-	grid $ds9(buttons,sep) -row 1 -column 3 -sticky ns
-    } else {
-	grid forget $ds9(buttons)
-	grid forget $ds9(buttons,sep)
+        grid $ds9(buttons,frame) -row 1 -column 5 -sticky ns
+        $ds9(buttons,sep) configure -orient vertical
+        grid $ds9(buttons,sep) -row 1 -column 6 -sticky ns
     }
+
+    # image
+    grid $ds9(image) -row 1 -column 2 -sticky news
 }
+
